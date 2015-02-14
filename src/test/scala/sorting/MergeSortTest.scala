7758fcd20b0e7eeca62a79b18c1e05b552e6705c
ramn <github@rymdimperiet.org>
2015-02-14 23:12:53 +0100

    MergeSort: Test more collection types as input

--- src/test/scala/sorting/MergeSortTest.scala
+++ src/test/scala/sorting/MergeSortTest.scala
@@ -16,6 +16,18 @@ class MergeSortTest extends FunSuite {
     }
   }
 
+  test("accepts array as input") {
+    assertResult(Array(-0.1, 0.1, 1.2, 1.3, 2.0, 2.9)) {
+      MergeSort(Array(2.9, 1.2, -0.1, 1.3, 2.0, 0.1))
+    }
+  }
+
+  test("accepts Vector as input") {
+    assertResult(Vector(-0.1, 0.1, 1.2, 1.3, 2.0, 2.9)) {
+      MergeSort(Vector(2.9, 1.2, -0.1, 1.3, 2.0, 0.1))
+    }
+  }
+
   //test("big list") {
     //val xs = util.Random.shuffle(0 to 1000000).toList
     //assertResult(xs.sorted) {
