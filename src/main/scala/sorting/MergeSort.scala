edb22af1d7e187c012acd47463b6ff4bf4a7e6cf
ramn <github@rymdimperiet.org>
2015-02-14 20:14:25 +0100

    Make MergeSort more generic in collection type

--- src/main/scala/sorting/MergeSort.scala
+++ src/main/scala/sorting/MergeSort.scala
@@ -5,7 +5,7 @@ import scala.math.Ordering.Implicits._
 
 
 object MergeSort {
-  def apply[T : Numeric](xs: List[T]): List[T] = {
+  def apply[T : Numeric](xs: Seq[T]): Seq[T] = {
     val n = xs.length / 2
     if (n == 0)
       xs
@@ -17,23 +17,23 @@ object MergeSort {
 
   @tailrec
   private def merge[T : Numeric](
-    xs: List[T],
-    ys: List[T],
-    res: List[T]
-  ): List[T] =
+    xs: Seq[T],
+    ys: Seq[T],
+    res: Seq[T]
+  ): Seq[T] =
     (xs, ys) match {
       case (Nil, ys) => concatReverse(res, ys)
       case (xs, Nil) => concatReverse(res, xs)
-      case (x :: xtail, y :: ytail) =>
-        if (x < y) merge(xtail, ys, x :: res)
-        else merge(xs, ytail, y :: res)
+      case (x +: xtail, y +: ytail) =>
+        if (x < y) merge(xtail, ys, x +: res)
+        else merge(xs, ytail, y +: res)
     }
 
   @tailrec
-  private def concatReverse[T](xs: List[T], ys: List[T]): List[T] = {
+  private def concatReverse[T](xs: Seq[T], ys: Seq[T]): Seq[T] = {
     xs match {
       case Nil => ys
-      case head :: tail => concatReverse(tail, head +: ys)
+      case head +: tail => concatReverse(tail, head +: ys)
     }
   }
 }
