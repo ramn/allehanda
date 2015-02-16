bc729c253090b851886d3250cdb84e327012b033
ramn <github@rymdimperiet.org>
2015-02-16 01:22:32 +0100

    Add in place Quicksort

--- src/main/scala/sorting/QuickSort.scala
+++ src/main/scala/sorting/QuickSort.scala
@@ -1,7 +1,9 @@
 package se.ramn.sorting
 
-import annotation.tailrec
+import scala.annotation.tailrec
 import scala.math.Ordering.Implicits._
+import scala.util.Random
+import scala.reflect.ClassTag
 
 
 object QuickSort {
@@ -12,5 +14,68 @@ object QuickSort {
         apply(lessEq) ++ Seq(pivot) ++ apply(greater)
       case Seq() => unsorted
     }
+  }
+
+  def sortInplace[T : Ordering : ClassTag](unsorted: Array[T]): Unit = {
+    InplaceQuickSort.sortInplace(unsorted)
+  }
+}
+
+
+private object InplaceQuickSort {
+  def sortInplace[T : Ordering : ClassTag](unsorted: Array[T]): Unit = {
+    shuffle(unsorted)
+    sort(unsorted, 0, unsorted.length - 1)
+  }
+
+  private def shuffle[T](xs: Array[T]) = {
+    val len = xs.length
+    for (i <- 0 until len) {
+      exchange(xs, i, Random.nextInt(len))
+    }
+  }
+
+  private def sort[T : Ordering](xs: Array[T], lo: Int, hi: Int): Unit = {
+    if (hi > lo) {
+      val pivotIx = partition(xs, lo, hi)
+      require(pivotIx >= lo && pivotIx <= hi)
+      sort(xs, lo, pivotIx - 1)
+      sort(xs, pivotIx + 1, hi)
+    }
+  }
+
+  /*
+   * Partition the subarray with smaller items to the left of a pivot, larger
+   * to the right. Return pivot index.
+   * @param xs entire array to be sorted
+   * @param lo lower end subarray delimiter
+   * @param hi higher end subarray delimiter
+   * @return pivot index
+   */
+  private def partition[T : Ordering](xs: Array[T], initLo: Int, initHi: Int): Int = {
+    var lo = initLo + 1
+    var hi = initHi
+    val pivot = xs(initLo)
+    var pivotIx = initLo
+
+    while (lo <= hi) {
+      if (xs(lo) < pivot) {
+        exchange(xs, lo, pivotIx)
+        pivotIx += 1
+        lo += 1
+      } else if (xs(lo) > pivot) {
+        exchange(xs, lo, hi)
+        hi -= 1
+      } else {
+        lo += 1
+      }
+    }
+    pivotIx
+  }
+
+  private def exchange[T](xs: Array[T], i: Int, j: Int) = {
+    val tmp = xs(i)
+    xs(i) = xs(j)
+    xs(j) = tmp
   }
 }
--- src/test/scala/sorting/QuickSortTest.scala
+++ src/test/scala/sorting/QuickSortTest.scala
@@ -1,9 +1,10 @@
 package se.ramn.sorting
 
 import org.scalatest.FunSuite
+import org.scalatest.FunSpec
 
 
-class QuickSortTest extends FunSuite {
+class FunctionalQuickSortTest extends FunSuite {
   test("sorting of very small list of ints") {
     assertResult(List(2)) {
       QuickSort(List(2))
@@ -46,4 +47,27 @@ class QuickSortTest extends FunSuite {
       //QuickSort(xs)
     //}
   //}
+}
+
+
+class QuickSortInplaceTest extends FunSpec {
+  describe("In place Quicksort") {
+    it("should sort array of doubles") {
+      val original = Array(2.9, 1.2, -0.1, 1.3, 2.0, 0.1)
+      QuickSort.sortInplace(original)
+      assert(original === Array(-0.1, 0.1, 1.2, 1.3, 2.0, 2.9))
+    }
+
+    it("should sort tricky list") {
+      val original = List(9, 0, 3, 7, 6, 10, 2, 5, 4, 1, 8).toArray
+      QuickSort.sortInplace(original)
+      assert(original === original.sorted)
+    }
+
+    it("should sort big list") {
+      val original = util.Random.shuffle(0 to 10000).toArray
+      QuickSort.sortInplace(original)
+      assert(original === original.sorted)
+    }
+  }
 }
