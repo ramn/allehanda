eecea605c5e72e399f0328713935c8ebc7402e22
ramn <github@rymdimperiet.org>
2015-02-17 06:46:20 +0100

    Add performance benchmark for CountSort

--- src/test/scala/sorting/CountSortTest.scala
+++ src/test/scala/sorting/CountSortTest.scala
@@ -1,6 +1,7 @@
 package se.ramn.sorting
 
 import org.scalatest.FunSuite
+import org.scalameter.api._
 
 
 class CountSortTest extends FunSuite {
@@ -54,3 +55,23 @@ class CountSortTest extends FunSuite {
     //}
   //}
 }
+
+
+//object CountSortBenchmark extends PerformanceTest.Quickbenchmark {
+//  val sizes = Gen.range("size")(1000, 10000, 1000)
+//  val ranges = for (size <- sizes) yield 0 until size
+//
+//  def sort(r: Range) = {
+//    val ary: Array[Int] = r.toArray
+//    val original = util.Random.shuffle(ary.toSeq).toArray
+//    CountSort(original)
+//  }
+//
+//  performance of "CountSort" in {
+//    measure method "sort" in {
+//      using(ranges) in {
+//        r => sort(r)
+//      }
+//    }
+//  }
+//}
